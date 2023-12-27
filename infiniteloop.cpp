0d80d95f436215ef51c6c6f7b4a14559327683fe
Akash788965 <144426634+Akash788965@users.noreply.github.com>
2023-12-27 12:10:53 +0530

    Add files via upload

--- infiniteloop.cpp
+++ infiniteloop.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int i=1;
-    while(true){
-        cout<<i;
-        i++;
+
+        int *memory;
+
+        // Allocate 200 ints.
+
+        memory = malloc(200 * sizeof(int));
+
+        // Allocate 100 more ints.
+
+        // ERROR: This will compile, but will leave the previously
+
+        // allocated memory hanging, with no way to access it.
+
+        memory = malloc(100 * sizeof(int));
+
+        // Free second block of 100 ints.
+
+        // The first block is not freed.
+
+        free(memory);
+
+        return 0;
+
     }
-}
